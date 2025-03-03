4568f07d3f821e1206c9450d447f80461797eb42
stephanie rousset <strabon@club-internet.fr>
2025-03-03 11:12:01 +0100

    test: update factory and add system test

--- lib/decidim/admin_multi_factor/test/factories.rb
+++ lib/decidim/admin_multi_factor/test/factories.rb
@@ -5,7 +5,7 @@ require "decidim/core/test/factories"
 FactoryBot.define do
   factory :admin_multi_factor_component, parent: :component do
     name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :admin_multi_factor).i18n_name }
-    manifest_name :admin_multi_factor
+    manifest_name { :admin_multi_factor }
     participatory_space { create(:participatory_process, :with_steps) }
   end
 
